bf723af2f4c46f0fb718446198707082676978f2
adi <aklisiewicz01@gmail.com>
2022-06-18 23:45:42 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace procesorki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.axText = new System.Windows.Forms.TextBox();
+            this.bxText = new System.Windows.Forms.TextBox();
+            this.cxText = new System.Windows.Forms.TextBox();
+            this.dxText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fromButton = new System.Windows.Forms.Button();
+            this.toButton = new System.Windows.Forms.Button();
+            this.axFrom = new System.Windows.Forms.RadioButton();
+            this.bxFrom = new System.Windows.Forms.RadioButton();
+            this.cxFrom = new System.Windows.Forms.RadioButton();
+            this.dxFrom = new System.Windows.Forms.RadioButton();
+            this.axTo = new System.Windows.Forms.RadioButton();
+            this.bxTo = new System.Windows.Forms.RadioButton();
+            this.cxTo = new System.Windows.Forms.RadioButton();
+            this.dxTo = new System.Windows.Forms.RadioButton();
+            this.randomButton = new System.Windows.Forms.Button();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.xchgButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axText
+            // 
+            this.axText.Location = new System.Drawing.Point(68, 72);
+            this.axText.Name = "axText";
+            this.axText.Size = new System.Drawing.Size(100, 20);
+            this.axText.TabIndex = 0;
+            // 
+            // bxText
+            // 
+            this.bxText.Location = new System.Drawing.Point(68, 110);
+            this.bxText.Name = "bxText";
+            this.bxText.Size = new System.Drawing.Size(100, 20);
+            this.bxText.TabIndex = 1;
+            // 
+            // cxText
+            // 
+            this.cxText.Location = new System.Drawing.Point(68, 151);
+            this.cxText.Name = "cxText";
+            this.cxText.Size = new System.Drawing.Size(100, 20);
+            this.cxText.TabIndex = 2;
+            // 
+            // dxText
+            // 
+            this.dxText.Location = new System.Drawing.Point(68, 187);
+            this.dxText.Name = "dxText";
+            this.dxText.Size = new System.Drawing.Size(100, 20);
+            this.dxText.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(11, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "AX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(11, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "BX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(13, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CX";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "DX";
+            // 
+            // fromButton
+            // 
+            this.fromButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.fromButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.fromButton.Location = new System.Drawing.Point(283, 22);
+            this.fromButton.Name = "fromButton";
+            this.fromButton.Size = new System.Drawing.Size(82, 31);
+            this.fromButton.TabIndex = 8;
+            this.fromButton.Text = "FROM";
+            this.fromButton.UseVisualStyleBackColor = false;
+            // 
+            // toButton
+            // 
+            this.toButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.toButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.toButton.Location = new System.Drawing.Point(416, 22);
+            this.toButton.Name = "toButton";
+            this.toButton.Size = new System.Drawing.Size(82, 31);
+            this.toButton.TabIndex = 9;
+            this.toButton.Text = "TO";
+            this.toButton.UseVisualStyleBackColor = false;
+            // 
+            // axFrom
+            // 
+            this.axFrom.AutoSize = true;
+            this.axFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.axFrom.Location = new System.Drawing.Point(15, 9);
+            this.axFrom.Name = "axFrom";
+            this.axFrom.Size = new System.Drawing.Size(45, 20);
+            this.axFrom.TabIndex = 10;
+            this.axFrom.TabStop = true;
+            this.axFrom.Text = "AX";
+            this.axFrom.UseVisualStyleBackColor = true;
+            // 
+            // bxFrom
+            // 
+            this.bxFrom.AutoSize = true;
+            this.bxFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bxFrom.Location = new System.Drawing.Point(15, 47);
+            this.bxFrom.Name = "bxFrom";
+            this.bxFrom.Size = new System.Drawing.Size(45, 20);
+            this.bxFrom.TabIndex = 11;
+            this.bxFrom.TabStop = true;
+            this.bxFrom.Text = "BX";
+            this.bxFrom.UseVisualStyleBackColor = true;
+            // 
+            // cxFrom
+            // 
+            this.cxFrom.AutoSize = true;
+            this.cxFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cxFrom.Location = new System.Drawing.Point(15, 87);
+            this.cxFrom.Name = "cxFrom";
+            this.cxFrom.Size = new System.Drawing.Size(45, 20);
+            this.cxFrom.TabIndex = 12;
+            this.cxFrom.TabStop = true;
+            this.cxFrom.Text = "CX";
+            this.cxFrom.UseVisualStyleBackColor = true;
+            // 
+            // dxFrom
+            // 
+            this.dxFrom.AutoSize = true;
+            this.dxFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dxFrom.Location = new System.Drawing.Point(15, 128);
+            this.dxFrom.Name = "dxFrom";
+            this.dxFrom.Size = new System.Drawing.Size(46, 20);
+            this.dxFrom.TabIndex = 13;
+            this.dxFrom.TabStop = true;
+            this.dxFrom.Text = "DX";
+            this.dxFrom.UseVisualStyleBackColor = true;
+            // 
+            // axTo
+            // 
+            this.axTo.AutoSize = true;
+            this.axTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.axTo.Location = new System.Drawing.Point(20, 9);
+            this.axTo.Name = "axTo";
+            this.axTo.Size = new System.Drawing.Size(45, 20);
+            this.axTo.TabIndex = 14;
+            this.axTo.TabStop = true;
+            this.axTo.Text = "AX";
+            this.axTo.UseVisualStyleBackColor = true;
+            // 
+            // bxTo
+            // 
+            this.bxTo.AutoSize = true;
+            this.bxTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bxTo.Location = new System.Drawing.Point(20, 47);
+            this.bxTo.Name = "bxTo";
+            this.bxTo.Size = new System.Drawing.Size(45, 20);
+            this.bxTo.TabIndex = 15;
+            this.bxTo.TabStop = true;
+            this.bxTo.Text = "BX";
+            this.bxTo.UseVisualStyleBackColor = true;
+            // 
+            // cxTo
+            // 
+            this.cxTo.AutoSize = true;
+            this.cxTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cxTo.Location = new System.Drawing.Point(20, 87);
+            this.cxTo.Name = "cxTo";
+            this.cxTo.Size = new System.Drawing.Size(45, 20);
+            this.cxTo.TabIndex = 16;
+            this.cxTo.TabStop = true;
+            this.cxTo.Text = "CX";
+            this.cxTo.UseVisualStyleBackColor = true;
+            // 
+            // dxTo
+            // 
+            this.dxTo.AutoSize = true;
+            this.dxTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dxTo.Location = new System.Drawing.Point(19, 128);
+            this.dxTo.Name = "dxTo";
+            this.dxTo.Size = new System.Drawing.Size(46, 20);
+            this.dxTo.TabIndex = 17;
+            this.dxTo.TabStop = true;
+            this.dxTo.Text = "DX";
+            this.dxTo.UseVisualStyleBackColor = true;
+            // 
+            // randomButton
+            // 
+            this.randomButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.randomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.randomButton.Location = new System.Drawing.Point(56, 256);
+            this.randomButton.Name = "randomButton";
+            this.randomButton.Size = new System.Drawing.Size(112, 39);
+            this.randomButton.TabIndex = 18;
+            this.randomButton.Text = "RANDOM";
+            this.randomButton.UseVisualStyleBackColor = false;
+            this.randomButton.Click += new System.EventHandler(this.randomButton_Click);
+            // 
+            // moveButton
+            // 
+            this.moveButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.moveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.moveButton.Location = new System.Drawing.Point(217, 256);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(112, 39);
+            this.moveButton.TabIndex = 19;
+            this.moveButton.Text = "MOVE";
+            this.moveButton.UseVisualStyleBackColor = false;
+            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            // 
+            // xchgButton
+            // 
+            this.xchgButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.xchgButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.xchgButton.Location = new System.Drawing.Point(376, 256);
+            this.xchgButton.Name = "xchgButton";
+            this.xchgButton.Size = new System.Drawing.Size(112, 39);
+            this.xchgButton.TabIndex = 20;
+            this.xchgButton.Text = "XCHG";
+            this.xchgButton.UseVisualStyleBackColor = false;
+            this.xchgButton.Click += new System.EventHandler(this.xchgButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.axTo);
+            this.panel1.Controls.Add(this.bxTo);
+            this.panel1.Controls.Add(this.cxTo);
+            this.panel1.Controls.Add(this.dxTo);
+            this.panel1.Location = new System.Drawing.Point(416, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(80, 159);
+            this.panel1.TabIndex = 21;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.axFrom);
+            this.panel2.Controls.Add(this.bxFrom);
+            this.panel2.Controls.Add(this.cxFrom);
+            this.panel2.Controls.Add(this.dxFrom);
+            this.panel2.Location = new System.Drawing.Point(283, 59);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(80, 159);
+            this.panel2.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 337);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.xchgButton);
+            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.randomButton);
+            this.Controls.Add(this.toButton);
+            this.Controls.Add(this.fromButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dxText);
+            this.Controls.Add(this.cxText);
+            this.Controls.Add(this.bxText);
+            this.Controls.Add(this.axText);
+            this.Name = "Form1";
+            this.Text = "Intel 8086";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox axText;
+        private System.Windows.Forms.TextBox bxText;
+        private System.Windows.Forms.TextBox cxText;
+        private System.Windows.Forms.TextBox dxText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button fromButton;
+        private System.Windows.Forms.Button toButton;
+        private System.Windows.Forms.RadioButton axFrom;
+        private System.Windows.Forms.RadioButton bxFrom;
+        private System.Windows.Forms.RadioButton cxFrom;
+        private System.Windows.Forms.RadioButton dxFrom;
+        private System.Windows.Forms.RadioButton axTo;
+        private System.Windows.Forms.RadioButton bxTo;
+        private System.Windows.Forms.RadioButton cxTo;
+        private System.Windows.Forms.RadioButton dxTo;
+        private System.Windows.Forms.Button randomButton;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Button xchgButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace procesorki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public const string HexLetters = "0123456789abcdefABCDEF\b"; 
+
+
+
+        private void randomButton_Click(object sender, EventArgs e)
+        {
+        Random rnd = new Random();
+            axText.Text = rnd.Next(0, 65535).ToString("X");
+            bxText.Text = rnd.Next(0, 65535).ToString("X");
+            cxText.Text = rnd.Next(0, 65535).ToString("X");
+            dxText.Text = rnd.Next(0, 65535).ToString("X");
+        }
+
+        private void xchgButton_Click(object sender, EventArgs e)
+        {
+
+            if (axFrom.Checked && bxTo.Checked)
+            {
+                string x = axText.Text;
+                string y = bxText.Text;
+                axText.Text = y;
+                bxText.Text = x;
+            }
+            else if (axFrom.Checked && cxTo.Checked)
+            {
+                string x = axText.Text;
+                string y = cxText.Text;
+                axText.Text = y;
+                cxText.Text = x;
+            }
+            else if (axFrom.Checked && dxTo.Checked)
+            {
+                string x = axText.Text;
+                string y = dxText.Text;
+                axText.Text = y;
+                dxText.Text = x;
+            }
+
+            if (bxFrom.Checked && axTo.Checked)
+            {
+                string x = bxText.Text;
+                string y = axText.Text;
+                bxText.Text = y;
+                axText.Text = x;
+            }
+            else if (bxFrom.Checked && cxTo.Checked)
+            {
+                string x = bxText.Text;
+                string y = cxText.Text;
+                bxText.Text = y;
+                cxText.Text = x;
+            }
+            else if (bxFrom.Checked && dxTo.Checked)
+            {
+                string x = bxText.Text;
+                string y = dxText.Text;
+                bxText.Text = y;
+                dxText.Text = x;
+            }
+
+            if (cxFrom.Checked && axTo.Checked)
+            {
+                string x = cxText.Text;
+                string y = axText.Text;
+                cxText.Text = y;
+                axText.Text = x;
+            }
+            else if (cxFrom.Checked && bxTo.Checked)
+            {
+                string x = cxText.Text;
+                string y = bxText.Text;
+                cxText.Text = y;
+                bxText.Text = x;
+            }
+            else if (cxFrom.Checked && dxTo.Checked)
+            {
+                string x = cxText.Text;
+                string y = dxText.Text;
+                cxText.Text = y;
+                dxText.Text = x;
+            }
+
+            if (dxFrom.Checked && axTo.Checked)
+            {
+                string x = dxText.Text;
+                string y = axText.Text;
+                dxText.Text = y;
+                axText.Text = x;
+            }
+            else if (dxFrom.Checked && bxTo.Checked)
+            {
+                string x = dxText.Text;
+                string y = bxText.Text;
+                dxText.Text = y;
+                bxText.Text = x;
+            }
+            else if (dxFrom.Checked && cxTo.Checked)
+            {
+                string x = dxText.Text;
+                string y = cxText.Text;
+                dxText.Text = y;
+                cxText.Text = x;
+            }
+
+
+
+
+
+        }
+
+
+        private void moveButton_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            if (axFrom.Checked && bxTo.Checked)        
+            {
+                bxText.Text = axText.Text;
+            }
+            else if (axFrom.Checked && cxTo.Checked)
+            {
+                cxText.Text = axText.Text;
+            }
+            else if (axFrom.Checked && dxTo.Checked)
+            {
+                dxText.Text = axText.Text;
+            }
+
+
+
+            if (bxFrom.Checked && axTo.Checked)    
+            {
+                axText.Text = bxText.Text;
+            }
+            else if (bxFrom.Checked && cxTo.Checked)
+            {
+                cxText.Text = bxText.Text;
+            }
+            else if (bxFrom.Checked && dxTo.Checked)
+            {
+                dxText.Text = bxText.Text;
+            }
+
+
+
+            if (cxFrom.Checked && axTo.Checked)       
+            {
+                axText.Text = cxText.Text;
+            }
+            else if (cxFrom.Checked && bxTo.Checked)
+            {
+                bxText.Text = cxText.Text;
+            }
+            else if (cxFrom.Checked && dxTo.Checked)
+            {
+                dxText.Text = cxText.Text;
+            }
+            
+
+            if (dxFrom.Checked && axTo.Checked)        
+            {
+                axText.Text = dxText.Text;
+            }
+            else if (dxFrom.Checked && bxTo.Checked)
+            {
+                bxText.Text = dxText.Text;
+            }
+            else if (dxFrom.Checked && cxTo.Checked)
+            {
+                cxText.Text = dxText.Text;
+            }
+        }
+        
+    }
+}
